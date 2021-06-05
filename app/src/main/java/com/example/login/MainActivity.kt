ba4353783659f6c9cc3100ba03565b5a86647ff3
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-06-05 16:50:23 +0300

    third commit

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -9,39 +9,38 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etEmail : EditText
-    lateinit var etPassword : EditText
-    lateinit var btnLogin:Button
-    lateinit var btnSignUp:Button
+    lateinit var etEmail: EditText
+    lateinit var etPassword: EditText
+    lateinit var btnLogIn: Button
+    lateinit var btnSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         castViews2()
-        login()
+        check()
     }
-        fun castViews2(){
+
+
+    fun check() {
+
+        btnLogIn.setOnClickListener {
+            if (etEmail.text.toString().isEmpty()||(etPassword.text.toString().isEmpty())){
+                etEmail.setError("Email required")
+                etPassword.setError("Password required")
+            } else {
+                Toast.makeText(baseContext, "Welcome to my app", Toast.LENGTH_LONG).show()
+
+            }
+        }
+            btnSignUp.setOnClickListener {
+                var intent = Intent(baseContext, SignUp::class.java)
+                startActivity(intent)
+            }
+        }
+        fun castViews2() {
             etEmail = findViewById(R.id.etEmail)
             etPassword = findViewById(R.id.etPassword)
-            btnLogin=findViewById(R.id.btnLogIn)
-            btnSignUp=findViewById(R.id.btnSignUp)
-
-        }
-    fun login(){
-        btnLogin.setOnClickListener {
-            if (etEmail.text.toString().isEmpty()||etPassword.text.toString().isEmpty()){
-                etEmail.setError("Please input Email")
-                etPassword.setError("Please input password")
-
-            }else{
-                Toast.makeText(baseContext,"Welcome to my App",Toast.LENGTH_SHORT).show()
-            }
-
+            btnLogIn = findViewById(R.id.btnLogIn)
+            btnSignUp = findViewById(R.id.btnSignUp)
         }
     }
-
-}
-
-data class logInDetails(
-    var email: String,
-    var password: String
-)
